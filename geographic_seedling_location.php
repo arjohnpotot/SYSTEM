0a2f0383e06dfe94642fc349a4c53f90d8035a19
arjohnpotot <158052604+arjohnpotot@users.noreply.github.com>
2026-05-13 08:42:57 -0700

    Update geographic_seedling_location.php

--- geographic_seedling_location.php
+++ geographic_seedling_location.php
@@ -444,7 +444,7 @@ body {
 <!-- ===== SIDEBAR ===== -->
 <div class="sidebar">
     <div class="logo">
-        <a href="dashboard.php">
+        <a href="Dashboard.php">
             <img src="image/DENR.jpg" alt="DENR Logo">
         </a>
         <div class="sidebar-title">
@@ -453,7 +453,7 @@ body {
     </div>
 
     <nav>
-        <a href="dashboard.php" class="nav-link">
+        <a href="Dashboard.php" class="nav-link">
             <i class="fas fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
